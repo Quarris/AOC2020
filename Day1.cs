0aaaf15574ee85359ed3d90ecb29105d3232b3b2
Quarris <tomasz.winnicki@live.co.uk>
2020-12-01 12:38:24 +0000

    Improved Day 1

--- Day1.cs
+++ Day1.cs
@@ -5,36 +5,66 @@ namespace AOC2020 {
     public class Day1 : IDay {
 
         public void DoPart1(string[] inputs) {
-            int[] expenses = inputs.Select(int.Parse).ToArray();
-            for (int i = 0; i < expenses.Length; i++) {
-                for (int j = i + 1; j < expenses.Length; j++) {
-                    int firstExpense = expenses[i];
-                    int secondExpense = expenses[j];
-                    if (firstExpense + secondExpense == 2020) {
-                        Console.WriteLine($"The expenses which sum to 2020 are {firstExpense} and {secondExpense}");
-                        Console.WriteLine($"Their product is {firstExpense * secondExpense}");
-                        return;
-                    }
+            // Select => O(n) * O(1); OrderBy => O(nlogn) avg, O(n^2) worst;
+            int[] expenses = inputs.Select(int.Parse).OrderBy(i => i).ToArray();
+            int front = 0;
+            int back = expenses.Length - 1;
+
+            // O(n)
+            while (front < back) {
+                int sum = expenses[front] + expenses[back];
+                if (sum == 2020) {
+                    break;
+                }
+
+                if (sum > 2020) {
+                    back--;
+                } else {
+                    front++;
                 }
             }
+
+            if (expenses[front] + expenses[back] != 2020) {
+                throw new ArgumentException("Expenses do not contained any values which sum to 2020");
+            }
+
+            Console.WriteLine($"Expenses '{expenses[front]}' and '{expenses[back]}' sum to 2020");
+            Console.WriteLine($"Expense product results in {expenses[front] * expenses[back]}");
         }
 
         public void DoPart2(string[] inputs) {
-            int[] expenses = inputs.Select(int.Parse).ToArray();
-            for (int i = 0; i < expenses.Length; i++) {
-                for (int j = i + 1; j < expenses.Length; j++) {
-                    for (int k = j + 1; k < expenses.Length; k++) {
-                        int firstExpense = expenses[i];
-                        int secondExpense = expenses[j];
-                        int thirdExpense = expenses[k];
-                        if (firstExpense + secondExpense + thirdExpense == 2020) {
-                            Console.WriteLine($"The expenses which sum to 2020 are {firstExpense} and {secondExpense} and {thirdExpense}");
-                            Console.WriteLine($"Their product is {firstExpense * secondExpense * thirdExpense}");
-                            return;
-                        }
+            // Select => O(n) * O(1); OrderBy => O(nlogn) avg, O(n^2) worst;
+            int[] expenses = inputs.Select(int.Parse).OrderBy(i => i).ToArray();
+            int front = 0;
+            int middle;
+            int back = expenses.Length - 1;
+
+            // O(n)
+            for (middle = 1; middle < expenses.Length - 1; middle++) {
+                front = 0;
+                back = expenses.Length - 1;
+                // O(n)
+                while (front < back) {
+                    int sum = expenses[front] + expenses[middle] + expenses[back];
+                    if (sum == 2020) {
+                        goto foundSum;
+                    }
+
+                    if (sum > 2020) {
+                        back--;
+                    } else {
+                        front++;
                     }
                 }
             }
+
+            foundSum:
+            if (expenses[front] + expenses[middle] + expenses[back] != 2020) {
+                throw new ArgumentException("Expenses do not contained any values which sum to 2020");
+            }
+
+            Console.WriteLine($"Expenses '{expenses[front]}' and '{expenses[middle]}' and '{expenses[back]}' sum to 2020");
+            Console.WriteLine($"Expense product results in {expenses[front] * expenses[middle] * expenses[back]}");
         }
     }
 }
