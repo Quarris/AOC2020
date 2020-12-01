9e573f5d11c973e00b7d49ce5bef327859598b3c
Quarris <tomasz.winnicki@live.co.uk>
2020-12-01 11:33:22 +0000

    Day 1

--- Day1.cs
+++ Day1.cs
@@ -1,12 +1,40 @@
+using System;
+using System.Linq;
+
 namespace AOC2020 {
     public class Day1 : IDay {
 
         public void DoPart1(string[] inputs) {
-            
+            int[] expenses = inputs.Select(int.Parse).ToArray();
+            for (int i = 0; i < expenses.Length; i++) {
+                for (int j = i + 1; j < expenses.Length; j++) {
+                    int firstExpense = expenses[i];
+                    int secondExpense = expenses[j];
+                    if (firstExpense + secondExpense == 2020) {
+                        Console.WriteLine($"The expenses which sum to 2020 are {firstExpense} and {secondExpense}");
+                        Console.WriteLine($"Their product is {firstExpense * secondExpense}");
+                        return;
+                    }
+                }
+            }
         }
 
         public void DoPart2(string[] inputs) {
-            throw new System.NotImplementedException();
+            int[] expenses = inputs.Select(int.Parse).ToArray();
+            for (int i = 0; i < expenses.Length; i++) {
+                for (int j = i + 1; j < expenses.Length; j++) {
+                    for (int k = j + 1; k < expenses.Length; k++) {
+                        int firstExpense = expenses[i];
+                        int secondExpense = expenses[j];
+                        int thirdExpense = expenses[k];
+                        if (firstExpense + secondExpense + thirdExpense == 2020) {
+                            Console.WriteLine($"The expenses which sum to 2020 are {firstExpense} and {secondExpense} and {thirdExpense}");
+                            Console.WriteLine($"Their product is {firstExpense * secondExpense * thirdExpense}");
+                            return;
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace AOC2020 {
         };
 
         static void Main(string[] args) {
+            Boolean.TryParse(args[0], out bool isTest);
             int day;
             do {
-                Console.Write("Which day would you like to test?");
+                Console.WriteLine("Which day would you like to test?\n");
                 bool parsed = int.TryParse(Console.ReadLine(), out day);
                 if (!parsed || day < 1 || day > 25) {
                     Console.WriteLine("Incorrect day. Insert a day between 1 and 25 inclusive.");
@@ -18,19 +19,23 @@ namespace AOC2020 {
             } while (true);
 
             IDay solution = solutions[day - 1];
-            string[] inputs = GetInputs(day);
-            Console.WriteLine($"Are you ready to test day {day} part 1?");
+            string[] inputs = isTest ? GetTestInputs(day) : GetInputs(day);
+            Console.WriteLine($"Press any key to test day {day} part 1...");
             Console.ReadKey(true);
             Console.WriteLine($"Testing Part 2 of day {day}");
             solution.DoPart1(inputs);
-            Console.WriteLine($"Are you ready to test day {day} part 2?");
+            Console.WriteLine($"Press any key to test day {day} part 2...");
             Console.ReadKey(true);
             Console.WriteLine($"Testing Part 2 of day {day}");
             solution.DoPart2(inputs);
         }
 
         static string[] GetInputs(int day) {
-            return File.ReadAllLines($"Inputs/{day}");
+            return File.ReadAllLines($"Inputs/Day{day}.txt");
+        }
+
+        static string[] GetTestInputs(int day) {
+            return File.ReadAllLines($"Inputs/Day{day}Test.txt");
         }
     }
 }
